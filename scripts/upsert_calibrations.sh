378878f0188d258704fa4767deedac96c5b41264
Breath <bowtiedbreath@protonmail.com>
2023-10-07 10:35:56 -0400

    sports calibrationsg

--- scripts/upsert_calibrations.sh
+++ scripts/upsert_calibrations.sh
@@ -5,3 +5,7 @@ WORKDIR="$(git rev-parse --show-toplevel)"
 cargo run -r -p admin -- \
   -t calibrations \
   -f "$WORKDIR"/data/calibrations/movies.json
+
+cargo run -r -p admin -- \
+  -t calibrations \
+  -f "$WORKDIR"/data/calibrations/sports.json
